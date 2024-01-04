a403dd361e9b7ebb9d87579dfca86a94b1803205
jleeson <jleeson2000@gmail.com>
2024-01-04 11:25:53 -0800

    fix: fetch limit error

--- src/utils.js
+++ src/utils.js
@@ -54,7 +54,7 @@ export async function fetchChannelMessages(channel, options, limiter) {
     const messages = [];
 
     const messageCount = isNaN(options.maxMessagesPerChannel) ? 10 : options.maxMessagesPerChannel;
-    const fetchOptions = { limit: messageCount };
+    const fetchOptions = { limit: 100 };
 
     let lastMessageId;
     let fetchComplete = false;
